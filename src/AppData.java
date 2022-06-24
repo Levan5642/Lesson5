030141d4250503adbb69e88afa662def9b32fcc2
Levan5642 <91324000+Levan5642@users.noreply.github.com>
2022-06-24 12:17:31 +0300

    dz

--- src/AppData.java
+++ src/AppData.java
@@ -1,2 +1,71 @@
+import java.io.*;
+
 public class AppData {
+
+    private String[] headers;
+    private int[][] data;
+
+    public void read (File file) {
+        int size = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                size++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        headers = new String[3];
+        data = new int[size - 1][];
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            headers = bufferedReader.readLine().split(";");
+            int n = 0;
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+
+                String[] dataIntString = line.split(";");
+
+                int d1 = Integer.parseInt(dataIntString[0]);
+                int d2 = Integer.parseInt(dataIntString[1]);
+                int d3 = Integer.parseInt(dataIntString[2]);
+                data[n] = new int[] {d1, d2, d3};
+                n++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void write (File file) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(headers[0] + ";" + headers[1] + ";" + headers[2] + "\n");
+
+            for (int[] arr : data) {
+                bufferedWriter.write(arr[0] + ";" + arr[1] + ";" + arr[2] + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String[] getHeaders () {
+
+        return headers;
+    }
+
+    public void setHeaders (String[ ]headers){
+
+        this.headers = headers;
+    }
+
+    public int[][] getData () {
+
+        return data;
+    }
+    public void setData (int[][] data){
+
+        this.data = data;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AppData appData = new AppData();
+        File file = new File ("/Users/levankandasvili/Desktop/Lesson5/src/file.csv");
+        File file2 = new File ("/Users/levankandasvili/Desktop/Lesson5/src/file1.csv");
+
+        appData.read(file);
+
+        System.out.println(Arrays.toString(appData.getHeaders()));
+        System.out.println(Arrays.toString(appData.getData()[0]));
+        System.out.println(Arrays.toString(appData.getData()[1]));
+        System.out.println(Arrays.toString(appData.getData()[2]));
+        System.out.println(Arrays.toString(appData.getData()[3]));
+        System.out.println(Arrays.toString(appData.getData()[4]));
+        System.out.println(Arrays.toString(appData.getData()[5]));
+
+        appData.write(file2);
     }
+
 }
